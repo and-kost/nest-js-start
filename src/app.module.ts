d5d86f368252b39bac2d03bfa87ed783398d353a
andrey_kostyuchenko <andrey.kostyuchenko@dsr-corporation.com>
2022-10-09 15:36:07 +0300

    communication with db is added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from '@entities/user/user.module';
+import { TypeOrmModule } from '@db/typeorm.module'
+import { ConfigModule } from './config.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule,
+    TypeOrmModule,
+    UserModule
+  ]
 })
 export class AppModule {}
