5d5decec030709ade55569629729699875c465b8
andrey_kostyuchenko <andrey.kostyuchenko@dsr-corporation.com>
2022-10-09 14:23:28 +0300

    first test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Controller, Get, Post, Req, Res} from '@nestjs/common'
+import { AppService } from './app.service'
+import { Request, Response } from 'express'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/')
+  getHello(
+    @Req() req: Request,
+    @Res() res: Response
+  ) {
+    //return this.appService.getHello()
+    console.log(req.query)
+    return res.status(201).send()
+  }
+
+  @Post('/')
+  setHello(
+    @Req() req: Request,
+    @Res() res: Response
+  ) {
+    //return this.appService.getHello()
+    console.log(req.query)
+    return res.status(201).send()
   }
 }
